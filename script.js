f62de500e6f060e03feef1502e15f5b1314aba6b
Wes Young <wesley.young@du.edu>
2021-01-27 13:59:32 -0700

    Added styling to intro page

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 //Loads github and linkedIn pages
 const githubLink = document.querySelector("#icon0");
 const linkedinLink = document.querySelector("#icon1");
+const openButton = document.querySelector("#openButton");
 
-githubLink.addEventListener("click", () => {
-  const url = "https://github.com/wyoung60";
-  window.open(url);
-});
+if (githubLink) {
+  githubLink.addEventListener("click", () => {
+    const url = "https://github.com/wyoung60";
+    window.open(url);
+  });
+}
 
-linkedinLink.addEventListener("click", () => {
-  const url = "https://www.linkedin.com/in/wesley-young-0953821b2";
-  window.open(url);
-});
+if (linkedinLink) {
+  linkedinLink.addEventListener("click", () => {
+    const url = "https://www.linkedin.com/in/wesley-young-0953821b2";
+    window.open(url);
+  });
+}
+
+if (openButton) {
+  openButton.addEventListener("click", function () {
+    console.log("here");
+    window.location = "index.html";
+  });
+}
